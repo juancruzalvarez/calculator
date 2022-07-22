a2a2c0129ec8bd63eea0edf896ab0c639ab62807
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2022-07-22 01:46:07 -0300

    eval

--- expression.hh
+++ expression.hh
@@ -2,37 +2,49 @@
 #define EXPRESSION_H
 #include <string>
 
+#include "operator.hh"
 #include "token.hh"
 
-
-class Expression{
+class Expression
+{
 public:
-    virtual double evaluate() = 0;
-    virtual std::string to_str() = 0;
-
+   virtual double evaluate() = 0;
+   virtual std::string to_str() = 0;
 };
 
-class BinaryExpression : public Expression { 
+class BinaryExpression : public Expression
+{
 public:
-    BinaryExpression(Operator op, Expression* lhs, Expression* rhs);
-    double evaluate();
-    std::string to_str();
+   BinaryExpression(Operator op, Expression *lhs, Expression *rhs);
+   double evaluate();
+   std::string to_str();
 
+private:
+   Operator op;
+   Expression *lhs, *rhs;
 };
 
-class UnaryExpression : public Expression {
+class UnaryExpression : public Expression
+{
 public:
-    UnaryExpression(Operator op, Expression* lhs);
-    double evaluate();
-    std::string to_str();
+   UnaryExpression(Operator op, Expression *lhs);
+   double evaluate();
+   std::string to_str();
+
+private:
+   Operator op;
+   Expression *lhs;
 };
 
-class NumberExpression : public Expression {
+class NumberExpression : public Expression
+{
 public:
-    NumberExpression(double num);
-    double evaluate();
-    std::string to_str();
+   NumberExpression(double num);
+   double evaluate();
+   std::string to_str();
+
+private:
+   double num;
 };
 
-
-#endif 
+#endif
